5083a3c6850472eb00b1c462c26e7aaf4a363660
Tim Acker <tjacker31@gmail.com>
2023-06-04 15:10:37 -0400

    adds snippet text as innerHTML and changes title to page link

--- src/app/results-list/results-list.component.ts
+++ src/app/results-list/results-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { WikiSearchResult } from '../wiki-search.model';
 
 @Component({
 	selector: 'app-results-list',
 	templateUrl: './results-list.component.html',
 	styleUrls: ['./results-list.component.scss'],
+	encapsulation: ViewEncapsulation.None,
 })
 export class ResultsListComponent {
+	public readonly wikiBaseURL = 'https://en.wikipedia.org?curid=';
+
 	@Input() public searchResults: WikiSearchResult[];
 }
